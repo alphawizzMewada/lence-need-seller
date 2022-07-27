7e2125eb76b71026d71ee3f0cad7bde598d0620a
alphawizzMewada <surendraalphawizz@gmail.com>
2022-07-27 20:16:01 +0530

    first commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,22 @@
+final String appName = 'Lens Need Seller';
+
+final String packageName = 'com.zuqzuq.seller';
+
+final String iosPackage = 'com.zuqzuq.seller';
+
+// final String baseUrl = 'https://vendor.eshopweb.store/seller/app/v1/api/';
+//
+// final String jwtKey = "68f05dec6014f68e760c5c5fa3e31bcf391a2e10";
+
+final String baseUrl = 'https://alphawizztest.tk/Lensneed/seller/app/v1/api/';
+
+final String jwtKey = "78084f1698c9fcff5a668b68dcd103db39be2605";
+
+//timeout limit
+final int timeOut = 50;
+const int perPage = 10;
+
+const String BANK_DETAIL = 'Bank Details:\n' +
+    'Account No :123XXXXX\n' +
+    'IFSC Code: 123XXX \n' +
+    'Name: Abc xyz';
